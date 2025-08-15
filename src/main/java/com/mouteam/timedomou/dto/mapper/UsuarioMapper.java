bdf2109c6d5eb01d3a9632ac059aa0e1ce708c67
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-08-14 22:02:37 -0300

    🔧 chore: adiciona dependências modelMapper no pom.xml

---
+++ src/main/java/com/mouteam/timedomou/dto/mapper/UsuarioMapper.java
@@ -0,0 +1,10 @@
+package com.mouteam.timedomou.dto.mapper;
+
+import com.mouteam.timedomou.entity.Usuario;
+
+public class UsuarioMapper {
+    public static Usuario ToUsuario(Usuario){
+        return new Mode
+    }
+
+}
