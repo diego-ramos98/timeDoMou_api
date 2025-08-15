375fed127d7aa7033d823863c2af20b6e98c50d1
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-08-14 22:17:30 -0300

    ✨ feat(validação): Adicionando validações

--- src/main/java/com/mouteam/timedomou/dto/UsuarioCreateDto.java
+++ src/main/java/com/mouteam/timedomou/dto/UsuarioCreateDto.java
@@ -1,6 +1,10 @@
 package com.mouteam.timedomou.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
@@ -10,9 +14,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @ToString
 public class UsuarioCreateDto {
+
+    @NotBlank
+    @Size(max = 155)
     private String nome;
+
+    @NotBlank
     private String dataNasciemento;
+
+    @NotBlank
+    @CPF
     private String cpf;
+
+    @NotBlank
+    @Email(regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$",message = "Formato do e-mail está invalido")
     private String email;
+
+    @Size(min = 8,max = 8)
     private String senha;
 }
