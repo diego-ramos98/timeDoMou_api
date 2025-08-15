3fd21c8cf80c0926104ad6a3bf59f57c3f1826fa
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-08-14 21:34:32 -0300

    ✨ feat(controller): adiciona entidade de usuario

--- src/main/java/com/mouteam/timedomou/entity/Usuario.java
+++ src/main/java/com/mouteam/timedomou/entity/Usuario.java
@@ -1,4 +1,50 @@
 package com.mouteam.timedomou.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "noem")
+    private String nome;
+    @Column(name = "data_nascimento")
+    private LocalDate dataNascimento;
+    @Column(name = "cpf")
+    private String cpf;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "senha")
+    private String senha;
+    @Column(name = "perfil_usuario")
+    private Perfil perfilUsuario = Perfil.PERFIL_CLIENTE;
+    @Column(name = "status_usuario")
+    private boolean statusUsuario;
+    @Column(name = "data_inclusao")
+    private LocalDate dataInclusao;
+    @Column(name = "data_alteracao")
+    private LocalDate dataAlteracao;
+    @Column(name = "alterado_por")
+    private String  alteradoPor;
+
+
+    public enum Perfil{
+        PERFIL_ADMINISTRADOR,
+        PERFIL_CLIENTE
+    }
+
+
+
 }
