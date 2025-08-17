369ab242c36202dbfcfc2fa4147b9569df24c126
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-08-17 10:43:40 -0300

    ✨ feat(repository): Adiciona interface repositorio de treino

---
+++ src/main/java/com/mouteam/timedomou/repository/TreinoRepository.java
@@ -0,0 +1,7 @@
+package com.mouteam.timedomou.repository;
+
+import com.mouteam.timedomou.entity.Treino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TreinoRepository extends JpaRepository<Treino,Long> {
+}
