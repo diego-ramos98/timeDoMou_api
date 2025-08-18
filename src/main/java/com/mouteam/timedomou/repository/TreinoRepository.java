c61a1073e81987821389b0f80f2ee59bd421a924
diego-ramos98 <diego.ramos.fortunato@outlook.com>
2025-08-18 18:59:46 -0300

    ✨ feat(repository): criando repositorio para treino

--- src/main/java/com/mouteam/timedomou/repository/TreinoRepository.java
+++ src/main/java/com/mouteam/timedomou/repository/TreinoRepository.java
@@ -4,4 +4,5 @@ import com.mouteam.timedomou.entity.Treino;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TreinoRepository extends JpaRepository<Treino,Long> {
+
 }
